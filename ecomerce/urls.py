c45a7a1f5ef10fecdbccbc61ed8caef6d6393ff1
Bogdan Viorel Burlă <bogdanviorel13@gmail.com>
2023-09-13 16:16:14 +0000

    create urls.py in cart app and create cart.html file

--- ecomerce/urls.py
+++ ecomerce/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('', views.home, name='home'),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('cart/', include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
